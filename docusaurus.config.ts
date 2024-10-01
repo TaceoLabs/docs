5f3ed06dd7450ceadc76083ae9602a797f9d98c5
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-10-01 17:41:24 +0200

    build: temporary base-url for gh pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://docs.taceo.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docs/",
   trailingSlash: true,
 
   // GitHub pages deployment config.
