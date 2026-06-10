3011dcbedd96cffb83bca060e3b0b416ae664bed
Fabian Gruber <fabian.gruber5@gmail.com>
2026-06-10 09:43:52 +0200

    predicate compliance docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -189,6 +189,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['solidity'],
     },
     zoom: {
       selector: '.markdown img, .docusaurus-mermaid-container svg',
